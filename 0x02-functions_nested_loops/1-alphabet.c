57112e8105fa5ae7171b9ed47eb8f5d407b6e5db
Jeffrey James <lietsahara@gmail.com>
2023-09-13 07:10:22 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,18 @@
+#include "main.h"
+
+/**
+ * main -check code
+ *
+ * return - alphabet
+ */
 void print_alphabet(void)
 {
+	char letter;
 	letter = 'a';
 	while (letter <= 'z')
 	{
 		printf("%c",letter);
 		letter++;
 	}
-	printf(\n);
+	printf("\n");
 }

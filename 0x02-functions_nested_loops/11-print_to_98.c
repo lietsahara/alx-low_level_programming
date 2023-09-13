db53b6ecf12cf663e72de293e144865cf31bd1f1
Jeffrey James <lietsahara@gmail.com>
2023-09-13 00:16:33 -0700

    change some i ton

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,16 +3,16 @@
 void print_to_98(int n)
 {
 	int i;
-	while (i!=98)
+	while (n!=98)
 	{
-		if (i>98)
+		if (n>98)
 		{
 			for (i = n;i > 98;i++)
 			{	
 				printf("%d",i);
 			}
 		}
-		else if (i<98)
+		else if (n<98)
 		{
 		for (i = n;i < 98;i++)
 			{

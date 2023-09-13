9a9f1ea1adbaefe7f02f5e871bd38db621f941bc
Jeffrey James <lietsahara@gmail.com>
2023-09-12 23:24:24 -0700

    first print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+{
+	int i;
+	while (i!=98)
+	{
+		if (i>98)
+		{
+			for (i = n;i > 98;i++)
+			{	
+				printf("%d",i);
+			}
+		}
+		else if (i<98)
+		{
+		for (i = n;i < 98;i++)
+			{
+				printf("%d",i);
+			}
+		}
+	}
+
+	printf("98\n");
+}
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return 0;
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@ extern int a;
 extern int b;
 
 int add(int a,int b);
+void print_to_98(int n);
 
 #endif

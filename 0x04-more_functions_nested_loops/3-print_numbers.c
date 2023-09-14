12a4540cb6810117f8b268568c610fdf1cc45fe8
Jeffrey James <lietsahara@gmail.com>
2023-09-14 16:30:13 -0700

    added number 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+void ptint_numbers(void)
+{
+	char v;
+	v= '0';
+	
+	for (v = 0;v <= 9;v++)
+	{
+		putchar(v);
+	}
+	
+	return 0;
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,8 @@ extern int a;
 
 int print_square(int a);
 int  mul(int a,int b);
-int print_most_numbers(void);
+void print_most_numbers(void);
+void ptint_numbers(void);
+void print_line(int n);
 
 #endif

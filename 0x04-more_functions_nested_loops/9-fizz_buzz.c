cefe09f8190d8b75050b670c4cf96c045d95b512
Jeffrey James <lietsahara@gmail.com>
2023-09-14 15:58:55 -0700

    fizzbuzz done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*main - returns fizzbuzz sequence*/
+
+int main(void)
+{
+	int i = 1;
+	while(i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else 
+		{
+			printf("%d ",i);
+		}
+		i++;
+	}
+	return 0;
+}

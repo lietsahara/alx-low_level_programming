fd4761de24f6357b8a95195574d0270fb8773dfa
Jeffrey James <lietsahara@gmail.com>
2023-09-15 13:33:15 -0700

    print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description : 'the program outputs text values'
+* return : always return 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

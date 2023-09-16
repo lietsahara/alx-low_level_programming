dbfa03d6687e9feb9ec37b8f1e3ece306e5296b6
Jeffrey James <lietsahara@gmail.com>
2023-09-15 18:46:27 -0700

    changed char c to int c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,17 +2,18 @@
 #include "main.h"
 
 /**
- * main - cheks code
+ * print_most_numbers - prints from 1 to 9
  *
- * return - numbers from 1 to 9 apart from 2 and 4
+ * Return: numbers from 1 to 9 apart from 2 and 4
  */
 
 void print_most_numbers(void)
 {
-	char x = '1';
-	for (x = '1';x <= 9;x++)
+	int x = '1';
+
+	for (x = '1'; x <= 9; x++)
 	{
-		if (x == 2||x == 4)
+		if (x == '2' || x == '4')
 		{
 			continue;
 		}

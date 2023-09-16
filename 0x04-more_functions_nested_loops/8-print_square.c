b6d7f7b5fe02fe905a616268bebba89807ae5553
Jeffrey James <lietsahara@gmail.com>
2023-09-15 17:15:42 -0700

    print squarre

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,34 @@
 #include "main.h"
 
 /**
- * main- prints square
- *
- * return - squares
+ * print_square - prints square
+ * @a: input
+ * Description: uses putchar
+ * Return: 0
  */
 
 int print_square(int a)
 {
-	
-	return (a*a);
-	printf("\n");		
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	if (a <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < a; i++)
+		{
+			for (j = 0; j < a; j++)
+			{
+				putchar(35);
+			}
+			putchar('\n');
+		}
+	}
+	return (0);
 }

7041126adf3da8a0ce6a9df4da93e6f8437ad85c
Jeffrey James <lietsahara@gmail.com>
2023-09-19 18:00:03 -0700

    removed _putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,12 @@
+/**
+ * main.h - contains prototypes
+ */
+
 void puts_half(char *str);
 void puts2(char *str);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
